4c66907823244de14b40290736a1c9f04097de1b
Trương Minh <minhzany@gmail.com>
2022-11-22 23:52:40 +0700

    api login

--- src/services/userService.js
+++ src/services/userService.js
@@ -71,8 +71,36 @@ let checkUsername = (userName) => {
     })
 }
 
+let getAllUsers= (userId)=>{
+    return new Promise( async(resolve,reject)=>{
+        try {
+            let users='';
+            if(userId ==='ALL'){
+                users= await db.User.findAll({
+                    attributes:{
+                        exclude: ['password'] // để ẩm pw đi
+                    }
+                })
+            }
+            if(userId && userId !=='ALL'){
+                users= await db.User.findOne({
+                    where: { id:userId },
+                    attributes:{
+                        exclude: ['password'] // để ẩm pw đi
+                    }
+                })
+            }
+
+            resolve(users)
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 
 
 module.exports = {
     handleUserLogin: handleUserLogin,
+    getAllUsers:getAllUsers
 }
